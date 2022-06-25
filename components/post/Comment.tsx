0e01cc92d6b6742462bbd21ade3945ee26376719
ooooorobo <orobos654@gmail.com>
2022-06-26 00:04:41 +0900

    [feat] 댓글 다크모드

--- components/post/Comment.tsx
+++ components/post/Comment.tsx
@@ -1,25 +1,51 @@
-import {createRef, useEffect} from "react";
+import {createRef, useContext, useEffect} from "react";
+import {DarkModeContext} from "../../utils/context/DarkModeContext";
+import styled from "styled-components";
+
+const createUtterance = (themeName) => {
+    const wrapper = document.createElement('div')
+    const comment = document.createElement('script')
+    const config = {
+        src: "https://utteranc.es/client.js",
+        id: "utterance",
+        repo: "ooooorobo/ooooorobo.github.io",
+        'issue-term': "pathname",
+        label: "utterances",
+        theme: themeName,
+        crossOrigin: "anonymous",
+        async: "true"
+    }
+    Object.entries(config).forEach(([key, value]) => comment.setAttribute(key, value));
+    wrapper.appendChild(comment)
+    return wrapper;
+}
 
 export default function Comment() {
-    const commentRef = createRef<HTMLDivElement>();
+    const {isDarkMode} = useContext(DarkModeContext);
+    const lightRef = createRef<HTMLDivElement>();
+    const darkRef = createRef<HTMLDivElement>();
 
     useEffect(() => {
-        if (!commentRef.current?.firstChild) {
-            const ut = document.createElement('script');
-            const config = {
-                src: "https://utteranc.es/client.js",
-                id: "utterance",
-                repo: "ooooorobo/ooooorobo.github.io",
-                'issue-term': "pathname",
-                label: "utterances",
-                theme: "preferred-color-scheme",
-                crossOrigin: "anonymous",
-                async: "true"
-            }
-            Object.entries(config).forEach(([key, value]) => ut.setAttribute(key, value));
-            commentRef.current?.appendChild(ut);
+        if (!lightRef.current?.firstChild && !darkRef.current?.firstChild) {
+            const light = createUtterance('github-light');
+            const dark = createUtterance('github-dark');
+            lightRef.current?.appendChild(light)
+            darkRef.current?.appendChild(dark)
         }
     }, [])
 
-    return <div ref={commentRef}/>
+    return <Wrapper isDarkMode={isDarkMode}>
+        <div className={'dark'} ref={darkRef} />
+        <div className={'light'} ref={lightRef} />
+    </Wrapper>
 }
+
+const Wrapper = styled.div<{isDarkMode: boolean}>`
+    .light {
+      display: ${({isDarkMode}) => isDarkMode ? 'none' : 'block'};
+    }
+  
+  .dark {
+    display: ${({isDarkMode}) => isDarkMode ? 'block' : 'none'};
+  }
+`
