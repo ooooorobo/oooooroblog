85edada6cb78eb391b0b8565fcf88acbe4076c9c
ooooorobo <orobos654@gmail.com>
2022-06-25 21:36:14 +0900

    [fix] 포스트 페이지에서 다른 페이지로 이동해도 댓글 보이는 버그 수정
    ref: https://www.devhak.com/blog/react-add-utterances

--- components/post/Comment.tsx
+++ components/post/Comment.tsx
@@ -1,14 +1,25 @@
-import Script from "next/script";
+import {createRef, useEffect} from "react";
 
 export default function Comment() {
-    return <Script src="https://utteranc.es/client.js"
-                   id="utterance"
-        // @ts-ignore
-                   repo="ooooorobo/ooooorobo.github.io"
-                   issue-term="pathname"
-                   label="utterances"
-                   theme="preferred-color-scheme"
-                   crossOrigin="anonymous"
-                   async>
-    </Script>
+    const commentRef = createRef<HTMLDivElement>();
+
+    useEffect(() => {
+        if (!commentRef.current?.firstChild) {
+            const ut = document.createElement('script');
+            const config = {
+                src: "https://utteranc.es/client.js",
+                id: "utterance",
+                repo: "ooooorobo/ooooorobo.github.io",
+                'issue-term': "pathname",
+                label: "utterances",
+                theme: "preferred-color-scheme",
+                crossOrigin: "anonymous",
+                async: "true"
+            }
+            Object.entries(config).forEach(([key, value]) => ut.setAttribute(key, value));
+            commentRef.current?.appendChild(ut);
+        }
+    }, [])
+
+    return <div ref={commentRef}/>
 }
