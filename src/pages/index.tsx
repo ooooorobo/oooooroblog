2998924ed0a6c4a46c8a269eeebd121503fee817
ooooorobo <orobos654@gmail.com>
2022-06-27 01:17:00 +0900

    [feat] 메인의 포스트 리스트에서 다음 페이지 불러오기
    todo: 무한 스크롤 적용시키기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import { getPosts } from "@src/utils/postUtil";
 import { PostListElement } from "@src/model/post";
@@ -6,12 +7,28 @@ import Profile from "@src/components/main/Profile";
 import PostList from "@src/components/main/PostList";
 import WavyLine from "@src/components/WavyLine";
 
+const POST_COUNT = 10;
+
 const Home: NextPage<HomeProps> = ({ posts }: HomeProps) => {
+  const [page, setPage] = useState(0);
+  const [postList, setPostList] = useState<PostListElement[]>(posts);
+
+  const onClickNextPage = useCallback(() => setPage((prev) => prev + 1), []);
+
+  useEffect(() => {
+    if (page > 0) {
+      fetch(`/api/posts/${page}/${POST_COUNT}`)
+        .then((res) => res.json())
+        .then((json) => setPostList((prev) => [...prev, ...json]));
+    }
+  }, [page]);
+
   return (
     <Wrapper>
       <Profile />
       <WavyLine size={10} />
-      <PostList posts={posts} />
+      <PostList posts={postList} />
+      <button onClick={onClickNextPage}>다음</button>
     </Wrapper>
   );
 };
@@ -19,7 +36,7 @@ const Home: NextPage<HomeProps> = ({ posts }: HomeProps) => {
 export default Home;
 
 export const getStaticProps = async () => {
-  const posts: PostListElement[] = await getPosts(0, 10);
+  const posts: PostListElement[] = await getPosts(0, POST_COUNT);
   return { props: { posts } };
 };
 
