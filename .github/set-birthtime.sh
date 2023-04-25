c78e5b3c5d62da49495eb0b07a50ca47fa270c57
조예진 <orobos654@gmail.com>
2023-04-26 01:39:18 +0900

    Update set-birthtime.sh

--- .github/set-birthtime.sh
+++ .github/set-birthtime.sh
@@ -1,6 +1,5 @@
 git ls-tree -r --name-only main ./src/pages/posts | while read filename; do
-  unixtime=$(git log -1 --format="%at" -- "${filename}")
-  echo ${unixtime}
+  unixtime=$(git log --follow --format=%at --date default ${filename} | tail -1)
   touchtime=$(date -r ${unixtime} +'%Y%m%d%H%M.%S')
   touch -t ${touchtime} "${filename}"
-  done
+done
