da0fec4be4d3675b90bd4d90f08fb37296b2524f
조예진 <orobos654@gmail.com>
2023-11-17 04:03:36 +0900

    fix: fix client side exception
    mdx에 한글 포함될 시 swcMinify 켜면 한글 제대로 처리 못함..

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  swcMinify: true,
+  swcMinify: false,
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   compiler: {
@@ -10,16 +10,6 @@ const nextConfig = {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMdx = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    providerImportSource: "@mdx-js/react",
-  },
-});
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withContentlayer } = require("next-contentlayer");
 
-module.exports = withContentlayer(withMdx(nextConfig));
+module.exports = withContentlayer(nextConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,25 +9,9 @@ import { PostListElement } from "@src/model/post";
 import Profile from "@src/components/main/Profile";
 import PostList from "@src/components/main/PostList";
 
-const Home: NextPage = () => {
-  const posts = allPosts
-    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
-    .map(
-      (meta) =>
-        ({
-          slug: meta.url,
-          meta: {
-            index: meta.id,
-            title: meta.title,
-            description: meta.description || "",
-            postedAt: meta.date,
-            category: "",
-            series: "",
-            tags: [] as string[],
-          },
-        } as PostListElement)
-    );
+type HomeProps = { posts: PostListElement[] };
 
+const Home: NextPage<HomeProps> = ({ posts }: HomeProps) => {
   useEffect(() => {
     const scroll = parseInt(sessionStorage.getItem("mainscroll") ?? "0");
     window.scrollTo({ top: scroll, behavior: "auto" });
@@ -56,6 +40,31 @@ const Home: NextPage = () => {
       </div>
     </Wrapper>
   );
+};
+
+export const getStaticProps = () => {
+  const posts = allPosts
+    .sort((a, b) => {
+      const compareByDate = compareDesc(new Date(a.date), new Date(b.date));
+      if (compareByDate !== 0) return compareByDate;
+      return b.id - a.id;
+    })
+    .map(
+      (meta) =>
+        ({
+          slug: meta.url,
+          meta: {
+            index: meta.id,
+            title: meta.title,
+            description: meta.description || "",
+            postedAt: meta.date,
+            category: "",
+            series: "",
+            tags: [] as string[],
+          },
+        } as PostListElement)
+    );
+  return { props: { posts } };
 };
 
 export default Home;
