88784d5f8129c3386493c44bf8c48747a4cc599f
조예진 <orobos654@gmail.com>
2024-02-12 19:10:34 +0900

    chore: remove workflow

--- .github/set-birthtime.sh
+++
@@ -1,5 +0,0 @@
-git ls-tree -r --name-only main ./src/pages/posts | while read filename; do
-  unixtime=$(git log --follow --format=%at --date default ${filename} | tail -1)
-  touchtime=$(date -r ${unixtime} +'%Y%m%d%H%M.%S')
-  touch -t ${touchtime} "${filename}"
-done
